ea10e6fd6fdb1344d081227f5dd38fcbb768b54a
Ranto Uzumaki <rberk2011@gmail.com>
2020-02-26 10:50:00 +0530

    second commit

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageCrop,
+    FilePondPluginImageResize,
+    FilePondPluginImageTransform,
+    FilePondPluginImageEdit
+);
+
+FilePond.create(
+    document.getElementById('profileImg'),
+    {
+        labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
+        imagePreviewHeight: 170,
+        imageCropAspectRatio: '1:1',
+        imageResizeTargetWidth: 200,
+        imageResizeTargetHeight: 200,
+        stylePanelLayout: 'compact circle',
+        styleLoadIndicatorPosition: 'center bottom',
+        styleProgressIndicatorPosition: 'right bottom',
+        styleButtonRemoveItemPosition: 'left bottom',
+        styleButtonProcessItemPosition: 'right bottom',
+    }
+);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,76 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype>
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Project</title>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="css/filepond-plugin-image-preview.css" rel="stylesheet">
+        <link href="css/filepond-plugin-image-edit.css" rel="stylesheet">
+        <link href="css/filepond.css" rel="stylesheet">
+        <link href="css/logreg.css" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <body class='logRegBody'>
+        <div class='mainDiv'>
+            <div class='subDiv'>
+                <div class='regDiv'>
+                    <div class='regSubDiv'>
+                        <div class="regTitleDiv">
+                            <p class='regTitle'>Registeration</p>
+                        </div>
+                        <div class='profileDiv'>
+                            <div class='profileSubDiv'>
+                                <input type="file" class="filepond" name="filepond" id='profileImg' accept="image/png, image/jpeg, image/jpg"/>
+                            </div>
+                        </div>
+                        <div class='fnameDiv'>
+                            <input type="text" class='fname' name="firstname" id="firstname" placeholder="Enter Your First name...">
+                        </div>
+                        <div class='lnameDiv'>
+                            <input type="text" class='lname' name="lastname" id="lastname" placeholder="Enter Your last name...">
+                        </div>
+                        <div class='emailDiv'>
+                            <input type="email" class='emailCls' name="emailaddress" id="emailaddress" placeholder="Enter Your email here...">
+                        </div>
+                        <div class='phNumDiv'>
+                            <input type="number" class='phNum' name="phonenumber" id="phnum" placeholder="Enter Your phone number here...">
+                        </div>
+                        <div class='passDiv'>
+                            <input type="password" class='passCls' name="password" id="password" placeholder="Enter Your password here...">
+                        </div>
+                        <div class='confpassDiv'>
+                            <input type="password" class='confpass' name="confpassword" id="confpassword" placeholder="Enter Your repeat password here...">
+                        </div>
+                        <div class='regbtnDiv'>
+                            <button type="button" class='regbtn' id='regbtn'>Sign Up</button>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class='loginDiv'>
+                    <div class='loginSubDiv'>
+                        <div class='loginHeadDiv'>
+                            <p class='loginTitle'>Login</p>
+                        </div>
+                        <div class='logemailDiv'>
+                            <input type="text" class='logemail' name="email" id="loginemail" placeholder="Enter Your Email here...">
+                        </div>
+                        <div class='logpassDiv'>
+                            <input type="password" class='logpass' name="loginpassword" id="loginpassword" placeholder="Enter Your password here...">
+                        </div>
+                        <div class='loginbtnDiv'>
+                            <button type="button" class='loginbtn' id='loginbtn'>SignIn</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="js/filepond-plugin-image-crop.js"></script>
+        <script src="js/filepond-plugin-image-edit.js"></script>
+        <script src="js/filepond-plugin-image-preview.js"></script>
+        <script src="js/filepond-plugin-image-resize.js"></script>
+        <script src="js/filepond-plugin-image-transform.js"></script>
+        <script src="js/filepond.js"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
